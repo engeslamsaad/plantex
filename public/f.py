d69f4e2e321b1a936953030ac84572b3a3906636
eslamabu3esa <eslamabu3esa@gmail.com>
2024-11-25 14:37:15 +0200

    first commit

--- public/f.py
+++ public/f.py
@@ -1,6 +1,6 @@
 import os
 import absl.logging
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow Lite logs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # Suppress TensorFlow Lite logs
 absl.logging.set_verbosity(absl.logging.ERROR)  # Suppress absl logs
 
 import sys
