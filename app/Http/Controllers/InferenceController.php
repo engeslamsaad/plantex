44a9218058e8b3deca5c7ae0a1f8ca8a570dea13
eslamabu3esa <eslamabu3esa@gmail.com>
2024-11-25 16:20:25 +0200

    ad

--- app/Http/Controllers/InferenceController.php
+++ app/Http/Controllers/InferenceController.php
@@ -25,7 +25,7 @@ class InferenceController extends Controller
         $labelsPath = public_path('labels.txt');
         $imageFullPath = public_path("storage/$imagePath");
         
-        $command = escapeshellcmd("python3 /var/www/plantex/public/f.py $modelPath $labelsPath $imageFullPath  2>/dev/null");
+        $command = escapeshellcmd("python3 /var/www/plantex/public/f.py $modelPath $labelsPath $imageFullPath");
         $output = shell_exec($command);
 
         
