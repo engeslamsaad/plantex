0372b99345d3836aa18a25c5695c9e72ccaa929f
eslamabu3esa <eslamabu3esa@gmail.com>
2024-11-25 15:41:46 +0200

    first commit

--- app/Http/Controllers/InferenceController.php
+++ app/Http/Controllers/InferenceController.php
@@ -44,19 +44,20 @@ class InferenceController extends Controller
         $process = proc_open('python3 /var/www/plantex/public/f.py ' . escapeshellarg($modelPath) . ' ' . escapeshellarg($labelsPath) . ' ' . escapeshellarg($imageFullPath), $descriptorSpec, $pipes);
 
         if (is_resource($process)) {
-            // Read output from the pipe
             $output = stream_get_contents($pipes[0]);
             fclose($pipes[0]);
 
-            // Close the process
-            proc_close($process);
+            $return_value = proc_close($process);
 
-            // Extract "Grape__ww" from the output (assuming it's the first line)
+            if ($return_value !== 0) {
+                // Handle Python script error
+                return response()->json(['error' => 'Python script failed with exit code: ' . $return_value], 500);
+            }
+
+            // Extract label
             $label = trim(explode("\n", $output)[0]);
 
-            return response()->json([
-                'label' => $label,
-            ]);
+            return response()->json(['label' => $label]);
         } else {
             // Handle process opening error
             return response()->json(['error' => 'Failed to execute Python script'], 500);
